9178a32b1d6595f24c5c2cc8549c4a6b579dd729
ghassemiali <agh101981@gmail.com>
2022-01-21 00:34:59 +0330

    10-4

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login
-
+from django.contrib.auth.forms import AuthenticationForm
 
 def login_view(request):
-   if request.method == 'POST':
-      username = request.POST['username']
-      password = request.POST['Password']
-      print(username, password)
-      user = authenticate(request, username=username, password=password)
-      if user is not None:
-         login(request, user)
-         return redirect('/')
-         
-   return render(request, 'accounts/login.html')
+   if not request.user.is_authenticated:
+      if request.method == 'POST':
+         form = AuthenticationForm(request=request, data=request.POST)
+         if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']         
+            print(username, password)
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+               login(request, user)
+               return redirect('/')
+
+      form = AuthenticationForm()
+      content = {'form': form}  
+      return render(request, 'accounts/login.html', content)
+   else:
+      return redirect('/')
+
 
 #def logout_view(request):
 #    return 
