88ed191fe1a0ec9b6e6bc47025807f2a729bc601
ghassemiali <agh101981@gmail.com>
2022-01-22 15:15:58 +0330

    9-3

--- ghassemi7/settings.py
+++ ghassemi7/settings.py
@@ -41,10 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'django.contrib.sites',
     'django_extensions',
     'accounts.apps.AccountsConfig'
 ]
 
+SITE_ID = 2
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
